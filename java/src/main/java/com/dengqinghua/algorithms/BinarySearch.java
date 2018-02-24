401fdfe8a87418e496b3852006243df63a1f8cfb
dengqinghua <records_jy@163.com>
2018-02-24 17:33:12 +0800

    add BinarySearch

---
+++ java/src/main/java/com/dengqinghua/algorithms/BinarySearch.java
@@ -0,0 +1,45 @@
+package com.dengqinghua.algorithms;
+
+public class BinarySearch {
+    /**
+     * 二分查找 时间复杂度: <b>O(logn)</b>
+     *
+     * <pre>
+     *  假设需要查找 5
+     *
+     *     数组 datas      left  right index
+     *     1 3 5 8 9 10    0     5     3
+     *     1 3 5           0     2     1
+     *         5           2     2     2
+     *
+     *   注意 left, right 和 index 的变化情况, 尤其是 index, 她是 (leftIndex + rightIndex + 1)/2
+     * </pre>
+     *
+     * @param datas 数组对象, 要求已经是有序的
+     * @return 返回查找到的结果, 或者为null
+     */
+    public static boolean find(int[] datas, int dataToFind) {
+        int leftIndex = 0, rightIndex = datas.length - 1,
+                index = datas.length / 2;
+
+        while(rightIndex - leftIndex >= 0) {
+            if (leftIndex == rightIndex) {
+                return dataToFind == datas[index];
+            }
+
+            if (dataToFind == datas[index]) {
+                return true;
+            }
+
+            if (dataToFind < datas[index]) {
+                rightIndex = index - 1;
+            } else {
+                leftIndex = index + 1;
+            }
+
+            index = (leftIndex + rightIndex + 1) / 2;
+        }
+
+        return false;
+    }
+}
---
+++ java/src/test/java/com/dengqinghua/algorithms/BinarySearchTest.java
@@ -0,0 +1,27 @@
+package com.dengqinghua.algorithms;
+
+import org.junit.Test;
+import java.util.stream.IntStream;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class BinarySearchTest {
+    int[] setIntSortedArrays(int end) {
+        return IntStream.range(0, end).toArray();
+    }
+
+    @Test public void find() throws Exception {
+        int[] datas = setIntSortedArrays(1000);
+        assertTrue(BinarySearch.find(datas, 60));
+        assertTrue(BinarySearch.find(datas, 0));
+        assertFalse(BinarySearch.find(datas, 1001));
+    }
+
+    @Test public void find_Complicated_Case() throws Exception {
+        int[] datas = { 1, 3, 5, 8, 9, 10 };
+
+        assertTrue(BinarySearch.find(datas, 5));
+        assertFalse(BinarySearch.find(datas, 90));
+    }
+}
