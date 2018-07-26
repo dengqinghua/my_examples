c01f9767033efda89b8d5db06f3740fbf0319a71
dengqinghua <dengqinghua@didichuxing.com>
2018-07-26 21:36:46 +0800

    add imports test

--- golang/imports_test.go
+++ golang/imports_test.go
@@ -14,16 +14,15 @@ type forTest int
 func TestImports(t *testing.T) {
 	Convey("TestImports", t, func() {
 		Convey("should get current imports", func() {
-			res, _ := getBaiduContent("ds")
-			So(res.StatusCode, ShouldEqual, 200)
-			So(res.Status, ShouldEqual, "200 OK")
-
 			_, currentFileName, _, _ := runtime.Caller(0)
+			// 传入当前的文件名, 调用go/build 的ImportDir方法, 可以得到一个 context
 			context, err := build.ImportDir(path.Dir(currentFileName), 0)
 
 			if err != nil {
 				t.Log(err)
 			} else {
+				// context的Imports为当前包的依赖
+				// context的TestImports为当前包的测试依赖
 				SkipSo(context.Imports, ShouldResemble, []string{"errors", "math", "net/http", "strconv"})
 				SkipSo(context.TestImports, ShouldResemble, []string{"errors", "flag", "fmt",
 					"github.com/smartystreets/goconvey/convey", "github.com/stretchr/testify/mock",
