d0195e82b14f011c04a485bbc6ee9c79594000d5
dengqinghua <dengqinghua.42@gmail.com>
2019-05-04 22:08:44 +0800

    add docker example

---
+++ docker/sinatra/webapp/lib/app.rb
@@ -0,0 +1,16 @@
+require 'rubygems'
+require 'sinatra'
+require 'json'
+
+class App < Sinatra::Application
+
+  set :bind, '0.0.0.0'
+
+  get '/' do
+    "<h1>DockerBook Test Sinatra app</h1>"
+  end
+
+  post '/json/?' do
+    params.to_json
+  end
+end
