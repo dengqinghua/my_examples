a97d8b3eafb02c0796e5f389e8c7a609f13f78e9
dengqinghua <dengqinghua.42@gmail.com>
2019-09-01 22:36:58 +0800

    add rust examples

---
+++ rust/guessing_game/src/main.rs
@@ -0,0 +1,11 @@
+use std::io;
+
+fn main() {
+    println!("Guess the number!");
+    println!("please input your guess.");
+    let mut guess = String::new();
+    io::stdin().read_line(&mut guess).
+        expect("Fail to read line");
+
+    println!("You guessed {}", guess);
+}
