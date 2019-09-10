70d5221e69380c5ce15c2e79d69d2a29d1b35514
dengqinghua <dengqinghua.42@gmail.com>
2019-09-11 00:18:31 +0800

    make runC Code

--- golang/docker/container/container.go
+++ golang/docker/container/container.go
@@ -1,10 +1,25 @@
 package container
 
 import (
+	log "github.com/sirupsen/logrus"
 	"os"
 	"os/exec"
 	"syscall"
 )
+
+func RunControllerInitProcess(command string, args []string) error {
+	log.Infof("command %s", command)
+	defaultMountFlags := syscall.MS_NOEXEC | syscall.MS_NOSUID | syscall.MS_NODEV
+
+	syscall.Mount("proc", "/proc", "proc", uintptr(defaultMountFlags), "")
+
+	argv := []string{command}
+	if err := syscall.Exec(command, argv, os.Environ()); err != nil {
+		log.Errorf(err.Error())
+	}
+
+	return nil
+}
 
 func NewParentProcess(tty bool, command string) *exec.Cmd {
 	args := []string{"init", command}
--- rust/guessing_game/src/main.rs
+++ rust/guessing_game/src/main.rs
@@ -4,8 +4,9 @@ fn main() {
     println!("Guess the number!");
     println!("please input your guess.");
     let mut guess = String::new();
-    io::stdin().read_line(&mut guess).
-        expect("Fail to read line");
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Fail to read line");
 
     println!("You guessed {}", guess);
 }
