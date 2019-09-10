1a4129c13d881e6f25a03977d69874e3717871c5
dengqinghua <dengqinghua.42@gmail.com>
2019-09-10 16:56:35 +0000

    add init command

--- golang/docker/container/container.go
+++ golang/docker/container/container.go
@@ -9,13 +9,14 @@ import (
 
 func RunControllerInitProcess(command string, args []string) error {
 	log.Infof("command %s", command)
-	defaultMountFlags := syscall.MS_NOEXEC | syscall.MS_NOSUID | syscall.MS_NODEV
+	syscall.Mount("", "/", "", syscall.MS_PRIVATE|syscall.MS_REC, "")
 
+	defaultMountFlags := syscall.MS_NOEXEC | syscall.MS_NOSUID | syscall.MS_NODEV
 	syscall.Mount("proc", "/proc", "proc", uintptr(defaultMountFlags), "")
 
 	argv := []string{command}
 	if err := syscall.Exec(command, argv, os.Environ()); err != nil {
-		log.Errorf(err.Error())
+		log.Errorf("执行 syscall.Exec %s 报错: %s", command, err.Error())
 	}
 
 	return nil
